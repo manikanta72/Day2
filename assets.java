d00998a1eff4cf4c23f15c18e12683b7c1f513c0
SIVA MANIKANTA BASWA <baswamani67@gmail.com>
2024-12-10 20:28:15 +0530

    first commit

---
+++ assets.java
@@ -0,0 +1,12 @@
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class assets {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+    
+}
